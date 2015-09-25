c1d0aa97eec33b71fc351507efe1944ed0142a0c
jiawei <jaewei.j.w@gmail.com>
2015-09-26 00:58:09 +0800

    An pure front-end page that includes only a form to login:
    - two fields: email and password
    - one button: Login
    - both fields are required
    - password should be 8 characters long
    - the validation done using AngularJS

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('myApp', [])
+	.controller('InputController', ['$scope', function($scope) {
+		$scope.user = [];
+		$scope.email = null;
+		$scope.password = null;
+		$scope.submit = function() {
+			if ($scope.email && $scope.password) {
+				$scope.user = [];
+				$scope.user.push(this.email);
+				$scope.user.push(this.password);
+			}
+		};
+	}]);
